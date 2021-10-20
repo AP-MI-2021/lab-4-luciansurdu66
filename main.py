a0e5242e9f0d7b919bd2c4de5d64bb8837ca804f
luciansurdu66 <luciasurdu66@gmail.com>
2021-10-20 08:24:38 +0300

    Commit

--- main.py
+++ main.py
@@ -52,9 +52,53 @@ def test_sum_of_even_int():
     assert sum_of_even_int([2, 3, 12, 5, 9]) == 14
     assert sum_of_even_int([1, 15, 5, 9]) == 0
 
+
+# P4
+
+
+def show_even_numbers(lst: list[int]) -> list[int]:
+    """
+    Afiseaza o lista cu toate numerele pare
+    :param lst: lista de numere intregi
+    :return: o lista in care vor aparea doar o singura data numerele pare
+    """
+    newlist = []
+    for x in lst:
+        if x % 2 == 0 and x not in newlist:
+            newlist.append(x)
+    return newlist
+
+
+def test_show_even_numbers():
+    assert show_even_numbers([23, 12, 3, 52, 12]) == [12, 52]
+    assert show_even_numbers([23, 3, 33]) == []
+
+
+# P5
+
+
+def swap_numbers_with_sum(lst: list[int]):
+    """
+    Afiseaza lista obtinuta prin inlocuirea fiecarui numar cu un tuplu format din doua numere de pe
+     pozitii distincte din lista care adunate dau acel numar
+    :param lst: lista de numere intregi
+    :return: lista cu tuple
+    """
+    newlist = []
+    for x in lst:
+        
+    return newlist
+
+
+def test_swap_numbers_with_sum():
+    assert swap_numbers_with_sum([4, 8, 6, 3, 2, 1]) == [(3, 1), (6,2), (4, 2), (1, 2), 2, 1]
+
 def test_all():
     test_number_in_list()
     test_sum_of_even_int()
+    test_show_even_numbers()
+    test_swap_numbers_with_sum()
+
 
 def main():
     test_all()
@@ -63,6 +107,10 @@ def main():
         print("1. Citire lista de numere intregi")
         print("2. Verificati daca un numar se regaseste in lista incepand de la o anumita pozitie")
         print("3. Calculeaza suma tuturor numerelor intregi pare din lista")
+        print("4. Afiseaza toate numerele pare din lista. Daca se repeta un numar,"
+              "acesta va aparea in lista rezultat doar o singura data")
+        print("5. Afisati lista obtinuta prin inlocuirea fiecarui numar cu un tuplu format din doua numere"
+              "de pe pozitii distincte")
         print("x. Iesire")
         optiune = input(" Alegeti optiunea din meniu: ")
         if optiune == '1':
@@ -74,7 +122,9 @@ def main():
         elif optiune == '3':
             print(sum_of_even_int(lst))
         elif optiune == '4':
-            pass
+            print(show_even_numbers(lst))
+        elif optiune == '5':
+            print(swap_numbers_with_sum(lst))
         elif optiune == 'x':
             break
         else:
